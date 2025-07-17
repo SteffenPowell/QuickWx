ba0a19bc55f1a037fb629d7e2e69b5f486db2f49
SteffenPowell <steffen.powell@yahoo.com>
2025-07-17 17:56:53 -0500

    Add Flask startup block for Render

--- app.py
+++ app.py
@@ -281,3 +281,8 @@ def home():
         return render_template("briefing.html", output=output, route_coords=route_coords)
 
     return render_template("index.html")
+
+    return render_template("index.html")
+
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000)
